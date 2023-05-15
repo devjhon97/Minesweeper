fc802e8c64d44a312785c29e8e0e9537433012b9
devjhon97 <jhonatanvideos337@gmail.com>
2023-05-15 16:25:30 -0300

    project status 11%

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var board = document.getElementById("board");
 var btn = document.getElementById("btn");
 var nivel = document.getElementsByName("nivel");
 var start = document.getElementById('start');
+var classtiles = document.querySelector(".tiles");
 
 if(window.innerWidth < window.innerHeight){
     board.style.width = (window.innerWidth-20)+"px";
@@ -26,13 +27,15 @@ btn.onclick = () => {
 
 function createGame(area){
     start.style.display = "none";
-    board.style.display = "flex";
+    board.style.display = "block";
 
     let tiles = Array.from({length: area}, ()=> Array.from({length: area}, ()=> document.createElement("div")));
 
     for(let row = 0; row < tiles.length; row++){
         for(let column = 0; column < tiles[row].length; column++){
             tiles[row][column].classList.add("tiles");
+            classtiles.style.width = board.offsetWidth/area+"px";
+            classtiles.style.height = board.offsetHeight/area+"px";
             board.appendChild(tiles[row][column]);
         }
     }
